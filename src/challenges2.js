136a0dfc1ebdedea1fe3249f26d406d379629e49
gemaquejr <gemaquejr@hotmail.com>
2022-01-01 21:15:22 -0300

    Adiciona o Requisito 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,15 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(techs, name) {
+  if (techs.length === 0) return 'Vazio!';
+
+  let list = [];
+
+  techs.sort();
+  for (let value of techs) {
+    list.push({ tech: value, name });
+  }
+
+  return list;
 }
 
 // Desafio 11
@@ -9,8 +18,11 @@ function generatePhoneNumber() {
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  if (lineA + lineB < lineC || Math.abs(lineA - lineB) > lineC) {
+    return false;
+  }
+  return true;
 }
 
 // Desafio 13
